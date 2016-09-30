32fe2da0a9f49f414da0b50cf107c06b07dae335
solebox <solekiller@gmail.com>
2016-09-30 11:39:40 +0300

    added the make_readme_plugin_list twick for git links

--- make_readme_plugin_list.sh
+++ make_readme_plugin_list.sh
@@ -2,7 +2,7 @@
 pushd bundle > /dev/null 
 for plugin in `ls`;do
     pushd $plugin > /dev/null;
-    echo "* [$plugin]("$(echo $(git remote -v 2>&1) |sed 's/origin/moo/;'| awk '{print $2}')")"
+    echo "* [$plugin]("$(echo $(git remote -v 2>&1) |sed 's/git\:/https\:/;'| awk '{print $2}')")"
     popd 2>&1 > /dev/null 
 done
 popd 2>&1 > /dev/null 
